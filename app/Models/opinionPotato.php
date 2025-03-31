1604f514770457143207f6b194e1ff3134857258
aanddr3ii <161072980+aanddr3ii@users.noreply.github.com>
2025-03-31 20:11:05 +0200

    nivel 2
    tete

---
+++ app/Models/opinionPotato.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class opinionPotato extends Model
+{
+    use HasFactory;
+
+    protected $table = 'opinion_potato';
+
+    protected $fillable = [
+        'product_id',
+        'user_id',
+        'opinion',
+    ];
+
+    // Relaciones
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}

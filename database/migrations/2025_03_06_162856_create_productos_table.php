4f96078902ed989953018140e69e332d242754de
RaulSeguraIbanez <128618606+RaulSeguraIbanez@users.noreply.github.com>
2025-04-01 18:39:04 +0200

    a

--- database/migrations/2025_03_06_162856_create_productos_table.php
+++ database/migrations/2025_03_06_162856_create_productos_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->text('descripcion');
             $table->float('precio');
             $table->string('estado');
+            $table->string('ubicacion')->nullable();
             $table->boolean('oferta')->default(false);
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
